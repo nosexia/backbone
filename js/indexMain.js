7a1cbaf25c1dcd51f7220a2b514b2dc5428304f2
409903113@qq.com <409903113@qq.com>
2015-10-21 20:09:42 +0800

    实例化，调用初始化方法initialize

--- js/indexMain.js
+++ js/indexMain.js
@@ -10,40 +10,34 @@ define([
     var MyModel = backbone.Model;
 
     var MyView = backbone.View.extend({
-        initialize: function(){
-            this.listenTo(this.model, 'change:name', this.renderName);      //某个属性改变时，需要渲染
-            this.listenTo(this.model, 'change:age', this.renderAge);       
+        initialize: function(options){
+            this.listenTo(options.model, 'change', this.render);            //用options.model替换this.model
         },
-        renderName: function(){
-            console.log('变化name情况');
-        },
-        renderAge: function(age,age1,age3){
-            console.log('变化age情况');
-        } 
+        render: function(){
+            console.log('渲染所有');
+        }
     });
 
     var myModel = new MyModel();
     var myView = new MyView({
         model: myModel
     });
-    
 
-    $(document).click(function(){
-        myModel.set(person);
-    });
+    myModel.set(person);
 
-    $('.u-name').click(function(e){
-        person.name = new Date().getTime();  
-        e.stopPropagation();
-    });
 
-    $('.u-age').click(function(e){
-        person.age = new Date().getTime();
-        e.stopPropagation();
-    });
 });
 
-//监听模型里指定属性值变化
+/*var View = Backbone.View = function(options) {
+    this.cid = _.uniqueId('view');
+    _.extend(this, _.pick(options, viewOptions));
+    this._ensureElement();
+    this.initialize.apply(this, arguments);
+};*/
+
+//实例化时，会调用初始化方法，
+//this.initialize.apply(this, auguments);
+//实例化的实参传给initialize方法的形参
 
 
 
